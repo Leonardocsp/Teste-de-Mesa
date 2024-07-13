f53cfb4854da613b3e4ec7c876deb6d01799a3a8
Leonardocsp <147525997+Leonardocsp@users.noreply.github.com>
2024-07-13 16:45:31 -0300

    Add files via upload

---
+++ Parte3/Program.cs
@@ -0,0 +1,14 @@
+﻿using Ex1;
+using System;
+
+namespace Ex1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            JurosComposto juros = new JurosComposto();
+            juros.Juros();
+        }
+    }
+}
