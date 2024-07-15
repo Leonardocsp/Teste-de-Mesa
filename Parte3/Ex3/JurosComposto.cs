718b601acc0c1c5322d5624c69dde60059bd0923
Leonardocsp <147525997+Leonardocsp@users.noreply.github.com>
2024-07-15 09:10:41 -0300

    Update JurosComposto.cs

--- Parte3/Ex3/JurosComposto.cs
+++ Parte3/Ex3/JurosComposto.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,8 +22,9 @@ namespace Ex3
             Console.WriteLine($"Valor Presente: {valorPresente:F2}");
             Console.WriteLine();
 
-            Console.WriteLine("{0, -5} | {1, -15} | {2, -20}", "Mês", "Saldo", "Valor de Rendimento");
-            Console.WriteLine(new string('-', 46));
+            Console.WriteLine(new string('-', 48));
+            Console.WriteLine("|{0, -5} | {1, -15} | {2, -20}|", "Mês", "Saldo", "Valor de Rendimento");
+            Console.WriteLine(new string('-', 48));
 
             for (int i = 0; i < meses.Length; i++)
             {
@@ -31,8 +32,8 @@ namespace Ex3
                 meses[i] = valorFuturo;
                 rendimento = valorFuturo - valorPresente;
 
-                Console.WriteLine("{0, -5} | {1, -15:F2} | {2, -20:F2}", i, valorFuturo, rendimento);
-
+                Console.WriteLine("|{0, -5} | {1, -15:F2} | {2, -20:F2}|", i, valorFuturo, rendimento);
+                Console.WriteLine(new string('-', 48));
                 if (i == 5)
                 {
                     bool saqueValido = false;
@@ -59,6 +60,7 @@ namespace Ex3
                     }
                 }
             }
+            Console.WriteLine(new string('-', 48));
 
             for (int i = 0; i < 2; i++)
             {
@@ -66,7 +68,8 @@ namespace Ex3
                 meses[i] = valorFuturo;
                 rendimento = valorFuturo - valorPresente;
 
-                Console.WriteLine("{0, -5} | {1, -15:F2} | {2, -20:F2}", i + 6, valorFuturo, rendimento);
+                Console.WriteLine("|{0, -5} | {1, -15:F2} | {2, -20:F2}|", i + 6, valorFuturo, rendimento);
+                Console.WriteLine(new string('-', 48));
             }
 
             Console.ReadKey();
